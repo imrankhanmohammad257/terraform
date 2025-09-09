73078ff29339dbf25174938ce86f9cd33c0341f3
MOHAMMAD IMRAN KHAN <imrankhanmohammad257@gmail.com>
2025-09-09 16:36:18 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "filename" {
+  description = "The name of the file to create"
+  type        = string
+}
+
+variable "content" {
+  description = "The content to put inside the file"
+  type        = string
+}
